8b4c9d19b6d3f9deabf1e586ac427c40526946e4
subbulakshmi pachaiappan <105207582+SubbuPachaiappan@users.noreply.github.com>
2022-05-23 19:23:50 +0530

    File Reading

--- UsingBufferedReader.java
+++ UsingBufferedReader.java
@@ -1,35 +1,28 @@
-package filewritingoperations_23_5_2022;
+package filereadingoperation_23_5;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class UsingBufferedReader {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		// to create a file in this directory
-				String userdirectory = System.getProperty("user.dir");
 
-				// 1.need to give file location
-				String location = "BufferedWriting.txt";
-				// 2. write the content using string
-				String content = "Life is too simple";
-
-				String path = userdirectory + File.separator + "File" + File.separator + location;
-				//create filewriter class to initiate the file 
-				FileWriter writefile = new FileWriter(path);
-				//create bufferedwriter class , it does not connect the file directly
-				BufferedWriter bufferedwriter= new BufferedWriter(writefile);
-				// to write content
-				bufferedwriter.write(content);
-				// to finish content
-				bufferedwriter.close();
-				
-				System.out.println("file has been created");
-
-
+		String userDirectory = System.getProperty("user.dir");
+		String filename = "BufferedWriting.txt";
+		String path = userDirectory + File.separator + "File" + File.separator + filename;
+		// to read the particular file
+		FileReader fileReader = new FileReader(path);
+		//
+		BufferedReader fileread = new BufferedReader(fileReader);
+		// this is for to read the single line readLine()
+		String currentLine;
+		// to read multiple line
+		while ((currentLine = fileread.readLine()) != null) {
+			System.out.println(currentLine);
+		}
 	}
 
 }
